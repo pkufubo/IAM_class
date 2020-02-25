f84dff3cf62b2e27dde6397006a4e5b5094525a9
Bo Fu <42598161+pkufubo@users.noreply.github.com>
2020-02-25 10:45:22 +0800

    Add files via upload

--- find_k.py
+++ find_k.py
@@ -1,10 +1,37 @@
 def find_k(list,k):
+    list1=list.copy()
     if k<=100:#k较小时采用递归定义，复杂度k*n
         if k==1:
-            return max(list) 
+            return max(list1) 
         else:
-            list.remove(max(list))
-            return find_k(list,k-1)
+            list1.remove(max(list1))
+            return find_k(list1,k-1)
     else:#k较大时，先排序再取，复杂度由排序算法决定
         sorted(list)
         return list[-k]
+		
+def find_se(list):
+    start=[]
+    end=[]
+    for i in range(len(list)):
+        if list[i]!=0:
+            if i==0 or list[i-1]==0:
+                start.append(i)
+            if i==len(list)-1:
+                end.append(i)
+        else:
+            if i==len(list)-1 or list[i-1]!=0:
+                end.append(i-1)
+    return(start,end)
+	
+def main(list,k):
+    start,end=find_se(list) 
+    period=[end[i]-start[i]+1 for i in range(len(start))]#降水事件时间长度
+    pre=[sum(list[start[i]:end[i]+1]) for i in range(len(start))]#降水事件的总降雨量
+    f_event=[period[i]*pre[i] for i in range(len(start))]#计算f_event
+    #print(f_event)
+    #print(k)
+    return find_k(f_event,k)
+
+data=[1,0,0,1,2,0,4,0,3,5,6,0,0,0,0,1,0,5]
+print(main(data,2))
